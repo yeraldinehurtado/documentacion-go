570828616f28229dd233134af21c97372e4866d9
yeraldinehurtado <yeraldinehurtado1@gmail.com>
2022-11-02 17:08:20 -0500

    channels

--- 17_channels/comunicacion_entre_goroutines/main.go
+++ 17_channels/comunicacion_entre_goroutines/main.go
@@ -9,11 +9,14 @@ import (
 
 func main(){
 
-	//hacemos el waitgroup
+	//hacemos la variable de waitgroup
 	wg := &sync.WaitGroup{}
 
 	// creamos channel
 	IDsChan := make(chan string)
+
+	// cuantas goroutines vamos a aesperar
+	wg.Add(2)
 
 	//creamos las dos goroutines
 	go generateIDS(wg, IDsChan) //pasamos nuestro channel
@@ -24,20 +27,37 @@ func main(){
 }
 
 func generateIDS(wg *sync.WaitGroup, idsChan chan string){ // creamos goroutine
-	id := uuid.New() // generamos un id
+	// para enviar multiples valores de foma concurrente
 
-	//este valor que estamos recibiendo aqui, lo vamos a pasar al
-	//channel
-	idsChan <- id.String() // este id lo convertimos a tipo string
+	for i := 0; i < 100; i++ {
+		id := uuid.New() // generamos un id
 
+		//este valor que estamos recibiendo aqui, lo vamos a pasar al
+		//channel
+		//idsChan <- id.String() // este id lo convertimos a tipo string
+		idsChan <- fmt.Sprintf("%d, %s", i+1, id.String()) // vamos a mostrar indice + el id
+	}
+	
+
+	//cerramos nuestro channel para que no siga esperando valores
+	close(idsChan)
+
+
+	
 	wg.Done()
 }
 
 func logIDs(wg *sync.WaitGroup, idsChan chan string) { // creamos una nueva goroutine
-	id := <- idsChan // asi indicamos que el valor tipo string
+	//id := <- idsChan // asi indicamos que el valor tipo string
 	// que viene dentro del channel va a ser trasladado a 
 	// nuestra nueva variable id
 
-	fmt.Println(id) // mostramos el id
+	for id := range idsChan { //vamos a recorrer todos los valores que vengan en el channel y se lo vamos a pasar a id
+		fmt.Println(id)
+	}
+
+	//fmt.Println(id) // mostramos el id
+
+	wg.Done()
 	
 }
