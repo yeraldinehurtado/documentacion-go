eefa00ac5aa443030f37ba2abf2e968d40ea8d58
yeraldinehurtado <yeraldinehurtado1@gmail.com>
2022-11-07 15:13:01 -0500

    servidor

--- 27_servidor/main.go
+++ 27_servidor/main.go
@@ -8,15 +8,28 @@ import (
 
 func main() {
 
+	// mux es una ruta asociada a un handler
+	// mux obtiene la ruta como tambien el handler
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Hola)
+	mux.HandleFunc("/page", PaginaNoFunciona)
+	mux.HandleFunc("/saludar", Saludar)
+
 	//Router
-	http.HandleFunc("/", Hola)
-	http.HandleFunc("/page", PaginaNoFunciona)
-	http.HandleFunc("/saludar", Saludar)
+	//http.HandleFunc("/", Hola)
+	//http.HandleFunc("/page", PaginaNoFunciona)
+	//http.HandleFunc("/saludar", Saludar)
 
 	// Crear un servidor
+	server := &http.Server{
+		Addr:    "localhost:3000",
+		Handler: mux,
+	}
 	fmt.Println("El servidor está corriendo en el puerto 3000")
 	fmt.Println("Run server: http://localhost:3000/")
-	log.Fatal(http.ListenAndServe("localhost:3000", nil)) // ruta donde se va a ejecutar nuestro servidor
+	//log.Fatal(http.ListenAndServe("localhost:3000", mux)) // ruta donde se va a ejecutar nuestro servidor
+
+	log.Fatal(server.ListenAndServe())
 
 }
 
